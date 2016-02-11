ae79c777290ee167c1a4679a15561fbaba5c506b
Chance Johnson <chancejohnson@seattleacademy.org>
2016-02-11 13:04:20 -0800

    add changes

--- 03_chance.js
+++ 03_chance.js
@@ -1,13 +1,11 @@
-var net = require("net");
-//console.log("net" , net);
-
+port = process.argv[2];
+var port = (process.argv[2] ? Number(process.argv[2]) : 5073);
+var net = require('net');
 var server = net.createServer(respond);
 
-function respond(socket){
-	d = new Date();
-	socket.end(d.toString());
+function respond(socket) {
+    d = new Date();
+    socket.end(d.toString());
 }
-
-var port = process.argv[2];
-console.log("Listening on port" ,port);
+console.log("Listening on port ", port);
 server.listen(port);
--- 04_template.js
+++ 04_template.js
@@ -8,7 +8,7 @@ var pageName = pageName.substring(n + 1);
 pageName = pageName.replace(".js",".html");
 
 //if port not given use this as default
-var port = (process.argv[2] ? Number(process.argv[2]) : 3000);
+var port = (process.argv[2] ? Number(process.argv[2]) : 5020);
 app.listen(port);
 console.log("listening on port ", port);
 
